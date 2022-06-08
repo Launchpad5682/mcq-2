2bce6c9a279fbc4a2646271854a7c150fbc11efd
launchpad5682 <launchpad5682@gmail.com>
2022-06-08 22:07:44 +0530

    feat: cart count added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
+import { useDataProvider } from "../context/data-provider";
 
 export function Header() {
+  const { cart } = useDataProvider();
+
+  const count = cart.reduce((prev, curr) => prev + curr.quantity, 0);
+
   return (
     <header className="flex w-full bg-blue-700 items-center p-4">
       <NavLink
         to="/cart"
         className="text-white text-2xl items-center ml-auto flex"
       >
-        <AiOutlineShoppingCart /> Cart
+        <AiOutlineShoppingCart /> Cart ({count} items)
       </NavLink>
     </header>
   );
